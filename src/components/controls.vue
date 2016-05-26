dada898f5d96b9e6d60e9525187f4e5d309389f5
Thai Le <thai.le@vice.com>
2016-05-26 17:34:21 -0400

    changed control styles

--- src/components/controls.vue
+++ src/components/controls.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="controls center">
-    <button v-on:click.prevent="replayVideo" class="btn-floating btn-medium waves-effect waves-light">
+  <nav class="controls center grey darken-4">
+    <button v-on:click.prevent="replayVideo" class="btn-floating btn-medium waves-effect waves-light grey darken-1">
       <i class="material-icons">replay</i>
     </button>
 
-    <button v-on:click.prevent="togglePlayback" class="btn-floating btn-large waves-effect waves-light">
+    <button v-on:click.prevent="togglePlayback" class="btn-floating btn-large waves-effect waves-light pink ligthen-1">
       <i v-if="isPlaying" class="material-icons">pause</i>
       <i v-else class="material-icons">play_arrow</i>
     </button>
 
-    <button v-on:click.prevent="skipVideo" class="btn-floating btn-medium waves-effect waves-light">
+    <button v-on:click.prevent="skipVideo" class="btn-floating btn-medium waves-effect waves-light grey darken-1">
       <i class="material-icons">skip_next</i>
     </button>
-  </div>
+  </nav>
 </template>
 
 <script>
@@ -61,12 +61,18 @@
   }
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
   .controls {
+    height: auto;
     padding: 5% 0;
   }
 
+  nav i.material-icons {
+    height: auto;
+    line-height: 1em;
+  }
+
   button {
-    margin: 0 2%;
+    margin: 0 3%;
   }
 </style>
