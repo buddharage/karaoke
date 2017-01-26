ef5f166c30a698d865222d5c12ea82355607997f
Thai Le <thai.le@vice.com>
2017-01-25 19:19:16 -0500

    added random idle videos when none are in queue (only starts after first karaoke song in added and completed/skipped, styled title cards, added log for player errors

--- src/components/player.vue
+++ src/components/player.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-bind:class="{'has-video': currentVideo}" class="video-view">
+  <div class="video-view">
     <div v-if="showPreview" class="preview">
-        <h4>{{ currentVideo.performer }} is perfoming</h2>
+        <h4>{{ currentVideo.performer }} is perfoming</h4>
         <h1>{{ currentVideo.song.title }}
     </div>
 
@@ -9,15 +9,29 @@
       <div id="mainPlayer"></div>
     </div>
 
-    <div class="no-videos">
-      <h1>Put some songs in!</h1>
+    <div v-show="isIdle" class="idle-player">
+      <h1>Happy birthday, Lyd!</h1>
+      <h4>Add songs at karaoke.thaivietle.com</h4>
+    </div>
+
+    <div v-show="!currentVideo && !isIdle" class="no-videos">
+      <h1>Happy birthday, Lyd!</h1>
+      <h4>Add songs at karaoke.thaivietle.com</h4>
     </div>
   </div>
 </template>
 
 <script>
+  import config from '../../config';
   import log from '../helpers/log.js';
   import YoutubeIframeLoader from 'youtube-iframe';
+  import ytApi from 'youtube-api-simple';
+
+  // Set Youtube API object
+  var youtube =ytApi({
+    api: config.googleKey,
+    uri: 'https://www.googleapis.com/youtube/v3/'
+  });
 
   export default {
     name: 'player',
@@ -28,9 +42,12 @@
     },
     data() {
       return {
+        isIdle: false,
         isPlaying: false,
         player: null,
-        videoTimer: null
+        videoTimer: null,
+        idlePlaylistId: 'PLz1B0-xw_mY43IV1JM7NaG8OVi5L052jQ',
+        idlePlaylistLength: 0
       }
     },
     props: [
@@ -40,6 +57,18 @@
     ],
     mounted() {
       YoutubeIframeLoader.load(this.setPlayer);
+
+      // Get idle playlist length
+      youtube.playlistItems().list({
+        part: 'snippet',
+        playlistId: this.idlePlaylistId
+      }).then((data) => {
+        if(!data) {
+          return;
+        }
+
+        this.idlePlaylistLength = parseInt(JSON.parse(data).pageInfo.totalResults, 10);
+      });
     },
     watch: {
       currentVideo(newVal, oldVal) {
@@ -77,6 +106,8 @@
           this.db.ref().update({isPlaying: false});
 
           this.player.stopVideo();
+
+          this.playIdleVideo();
         }
       }
     },
@@ -84,14 +115,26 @@
       /**
        * onPlaybackChange() binds the Firebase `isPlaying` value to
        * determine if player should play or pause
+       *    -1 – unstarted
+       *    0 – ended
+       *    1 – playing
+       *    2 – paused
+       *    3 – buffering
+       *    5 – video cued
+       *
        * @param  {Object} snapshot  Firebase data
        */
       onPlaybackChange(snapshot) {
         this.isPlaying = snapshot.val();
 
-        if(this.isPlaying) {
+        if (this.isPlaying) {
+          log('[player] onPlaybackChange() this.player: ', this.player);
+
+          this.isIdle = false;
           this.player.playVideo();
-        } else if (this.player.getPlayerState() === 1) {
+
+          log('[player] onPlaybackChange() this.player: ', this.player.getVideoData());
+        } else if (this.player.getPlayerState() === 1 && !this.isIdle) {
           // Pause video only if video is playing
           this.player.pauseVideo();
         }
@@ -107,7 +150,7 @@
           this.player.seekTo(0);
 
           this.db.ref('isPlaying').on('value', (snapshot) => {
-            if(!snapshot.val()) {
+            if(!snapshot.val() && !this.isIdle) {
               this.player.pauseVideo();
             }
           })
@@ -122,7 +165,29 @@
 
         this.db.ref('videoPosition').on('value', this.onPositionChange);
       },
+      /**
+       * onPlayerError()
+       *
+       * ERROR CODES:
+       *
+       * 2 – The request contains an invalid parameter value. For example, this error occurs if you specify a video ID that does not have 11 characters, or if the video ID contains invalid characters, such as exclamation points or asterisks.
+       * 5 – The requested content cannot be played in an HTML5 player or another error related to the HTML5 player has occurred.
+       * 100 – The video requested was not found. This error occurs when a video has been removed (for any reason) or has been marked as private.
+       * 101 – The owner of the requested video does not allow it to be played in embedded players.
+       * 150 – This error is the same as 101. It's just a 101 error in disguise!
+       *
+       */
+      onPlayerError(e) {
+        log('[player] onPlayerError() e: ', e);
+
+        if ((e.data === 101 || e.data === 150) && this.isIdle) {
+          this.playIdleVideo();
+        }
+
+        log('[player] onPlayerError() this.player info: ', this.player.getVideoData());
+      },
       onPlayerStateChange(e) {
+        log('[player] onPlayerStateChange state: ', e.data);
         switch(e.data) {
           case 1: //playing
             this.trackTime();
@@ -135,11 +200,28 @@
             clearInterval(this.videoTimer);
         }
       },
+      playIdleVideo() {
+        var options = {
+          list: this.idlePlaylistId,
+          listType: 'playlist',
+          index: Math.floor(Math.random() * (this.idlePlaylistLength - 1))
+        };
+
+        log('[player] playFiller() options', options);
+
+        this.player.loadPlaylist(options);
+
+        this.isIdle = true;
+      },
       /**
        * playNext() plays next video in queue
        */
       playNext() {
         if(!this.currentVideo || !this.videos.length) {
+          if (this.isIdle) {
+            this.playIdleVideo();
+          }
+
           return;
         }
 
@@ -163,9 +245,9 @@
        */
       setPlayer(YT) {
         var youtubeOpts = {
+          videoId: 'QtxlCsVKkvY',
           playerVars: {
             autoplay: false,
-            videoId: 'eFfmE37xLX0',
             controls: 0,
             modestbranding: true,
             rel: 0,
@@ -176,6 +258,7 @@
           events: {
             'onReady': this.onPlayerReady,
             'onStateChange': this.onPlayerStateChange,
+            'onError': this.onPlayerError
           }
         }
 
@@ -236,7 +319,34 @@
 
 <style lang="sass" scoped>
   h1, h2, h3, h4, h5 {
-    font-weight: 700;
+    font-weight: 500;
+    margin: 0.2em 0;
+    position: relative;
+  }
+
+  .idle-player,
+  .no-videos {
+    h1 {
+      font-weight: 200;
+      font-size: 10vw;
+    }
+
+    h4 {
+      font-size: 5.2vw;
+    }
+  }
+
+  .idle-player {
+    align-items: center;
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 100vw;
   }
 
   .preview {
@@ -247,40 +357,43 @@
     flex-direction: column;
     height: 100vh;
     justify-content: center;
-    padding: 18%;
+    padding: 18% 3%;
     position: fixed;
     text-align: center;
     width: 100vw;
     z-index: 100;
+
+    h1 {
+      font-size: 9vw;
+    }
+
+    h4 {
+      color: white;
+      font-size: 4.3vw;
+      font-weight: 300;
+    }
 
     &:after {
       z-index: -1;
     }
   }
 
-  .has-video {
-    .no-videos {
-      display: none;
-    }
-
-    .video-container {
-      display: block;
-    }
-  }
-
-  .video-container {
-    display: none;
-  }
-
   .no-videos {
     align-items: center;
+    background: black;
     display: flex;
     color: #ec407a;
+    flex-direction: column;
     font-weight: 200;
     height: 100vh;
     justify-content: center;
+    left: 0;
     margin-top: 0;
+    position: absolute;
     text-align: center;
+    top: 0;
+    z-index: 100;
+    width: 100vw;
   }
 
   .video-view {
@@ -291,10 +404,6 @@
 
   .no-videos,
   .preview {
-    h1 {
-      position: relative;
-    }
-
     &:before {
       content: "";
       background-image: url(/images/lydia.jpg);
